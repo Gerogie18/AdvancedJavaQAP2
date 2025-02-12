b3fb3e2440413c5c48cef22a1d23db800a91393d
Gerogie18 <jennlyver@gmail.com>
2025-02-11 21:48:57 -0330

    Problem-3 finish Address class

--- src/Problem3/Address.java
+++ src/Problem3/Address.java
@@ -1,4 +1,39 @@
 package Problem3;
 
 public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Address() {
+        this.street = "Street Address";
+        this.city = "City";
+        this.state = "State";
+        this.zip = "Zip Code";
+    }
+
+    public Address(Address newAddress) {
+        this.street = newAddress.street;
+        this.city = newAddress.city;
+        this.state = newAddress.state;
+        this.zip = newAddress.zip;
+    }
+
+    public Address(String street, String city, String state, String zip) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public String toString() {
+        return String.format("""
+                Address [
+                Street: %s,
+                City:   %s,
+                State:  %s,
+                Zip:    %s ]
+                """, street, city, state, zip);
+    }
 }
