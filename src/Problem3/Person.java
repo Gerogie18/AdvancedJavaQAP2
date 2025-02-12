78388b5c3cdb90f6a61c7e571c3cf264bba25b32
Gerogie18 <jennlyver@gmail.com>
2025-02-11 21:50:42 -0330

    Problem 3 finish Person class

--- src/Problem3/Person.java
+++ src/Problem3/Person.java
@@ -1,4 +1,30 @@
 package Problem3;
 
 public class Person {
+    private String lastName;
+    private String firstName;
+    private Address home;
+
+    public Person () {
+        this.firstName = "First Name";
+        this.lastName = "Last Name";
+        this.home = new Address();
+    }
+
+    public Person (String firstName, String lastName, Address home) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.home = new Address(home);
+    }
+
+    public String toString () {
+        return String.format("""
+                Person [
+                First Name: %s,
+                Last Name:  %s,
+                Home: %s
+                """, firstName, lastName, home.toString());
+    }
+
 }
+
